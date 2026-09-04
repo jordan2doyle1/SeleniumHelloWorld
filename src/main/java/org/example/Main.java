865fb6dcb64fe3bd0fba3c7e6f0c9b5bb273fd8c
Jordan Doyle <jordandoyle100@gmail.com>
2026-09-04 16:47:29 +0100

    Removed unnecessary dependencies and reduced code to simplest form.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,17 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        RemoteWebDriver driver = new ChromeDriver(new ChromeOptions());
+        WebDriver driver = new ChromeDriver(new ChromeOptions());
 
         driver.get("https://www.selenium.dev/selenium/web/blank.html");
 
         driver.close();
         driver.quit();
-
-        System.out.println("Successfully launched browser and quit");
     }
 }
